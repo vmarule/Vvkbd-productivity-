4fe1c3a5fba7c45119935823377b5e61580d006c
vmarule <vmarule@gmail.com>
2026-09-06 07:01:53 +0200

    Use versioned network-first cache for app code

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const C='vvkbd-full-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='vvkbd-full-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin && (url.pathname.endsWith('/index.html')||url.pathname.endsWith('.js'))){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  }else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
